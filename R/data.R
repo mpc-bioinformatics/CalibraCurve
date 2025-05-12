3ffe2a0d7b001dae51446a0dfacc018a35578dff
KarinSchork <karin.schork@rub.de>
2025-05-12 14:40:31 +0200

    begin docu for test data sets

---
+++ R/data.R
@@ -0,0 +1,41 @@
+#' Targeted Proteomics data set Albumin
+#'
+#' Details TODO
+#'
+#' @format ## `D_ALB`
+#' A data frame with 21 rows and 2 columns:
+#' \describe{
+#'   \item{Concentration}{Concentration}
+#'   \item{Measurement}{Measurement}
+#' }
+#' @source TODO
+"D_ALB"
+
+
+
+#' Targeted Proteomics data set Apolipoprotein
+#'
+#' Details TODO
+#'
+#' @format ## `D_Apolipoprotein`
+#' A data frame with 21 rows and 2 columns:
+#' \describe{
+#'   \item{Concentration}{Concentration}
+#'   \item{Measurement}{Measurement}
+#' }
+#' @source TODO
+"D_Apolipoprotein"
+
+
+#' Targeted Proteomics data set MFAP4
+#'
+#' Details TODO
+#'
+#' @format ## `D_MFAP4`
+#' A data frame with 55 rows and 2 columns:
+#' \describe{
+#'   \item{Concentration}{Concentration}
+#'   \item{Measurement}{Measurement}
+#' }
+#' @source TODO
+"D_MFAP4"
