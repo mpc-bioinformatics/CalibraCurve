4599d6dc38896090a7a3f3ab6eb61c77832e8ca0
KarinSchork <karin.schork@rub.de>
2025-05-16 11:25:08 +0200

    fix testing

--- tests/testthat/test-05_writing_results.R
+++ tests/testthat/test-05_writing_results.R
@@ -29,18 +29,20 @@ test_that("assemble result tables", {
   expect_equal(names(tables), c("result_table_conc_levels", "result_table_obs"))
 
   expect_equal(nrow(tables$result_table_conc_levels), 11)
-  expect_equal(ncol(tables$result_table_conc_levels), 13)
+  expect_equal(ncol(tables$result_table_conc_levels), 14)
   expect_equal(colnames(tables$result_table_conc_levels),
                c("substance", "concentration", "mean_measurement", "estimated_measurement",
+                 "removed_while_cleaning",
                  "CV", "CV_within_thres", "preliminary_linear_range", "mean_percentage_bias",
                  "SD_percentage_bias", "CV_percentage_bias", "mean_response_factor",
                  "RF_within_thres", "final_linear_range"))
 
 
   expect_equal(nrow(tables$result_table_obs), 11*5)
-  expect_equal(ncol(tables$result_table_obs), 7)
+  expect_equal(ncol(tables$result_table_obs), 8)
   expect_equal(colnames(tables$result_table_obs),
-               c("substance", "concentration", "measurement", "percentage_bias", "response_factor",
+               c("substance", "concentration", "measurement", "removed_while_cleaning",
+                 "percentage_bias", "response_factor",
                  "RF_within_thres", "final_linear_range"))
 })
 
